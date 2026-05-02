c05ad351b0dffc4b45ccdc0b13b406a3e72eb673
Alexdj15 <alexo.dejong@outlook.com>
2026-05-02 15:48:22 +1200

    Level 3

---
+++ trugtrug/scenes/level_3_world.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var can_respect = false
+var can_win = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Crabzer.visible = false
+	$win.visible = false
+	can_respect = false
+	can_win = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if can_respect and Input.is_action_just_pressed("respect"):
+		can_respect = false
+		$respect.play(30)
+		$Crabzer.visible = false
+		GlobalManager.level3_passed = true
+		$win_timer.start()
+	if can_win and Input.is_action_pressed("level_exit"):
+		get_tree().change_scene_to_file("res://scenes/levels.tscn")
+		
+
+func _on_crabzer_area_body_entered(body: Node2D) -> void:
+	$Crabzer.visible = true
+	can_respect = true
+
+
+func _on_win_timer_timeout() -> void:
+	$win.visible = true
+	can_win = true
