b8afb8c24c6c68a4e08a9059d37c9d9ceb63939e
Alexdj15 <alexo.dejong@outlook.com>
2026-05-02 14:21:19 +1200

    Coyote jump

--- trugtrug/scenes/trug (2).gd
+++ trugtrug/scenes/trug (2).gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var able_to_jump = true
+var was_on_floor = false
 
 func _ready() -> void:
 	$Stick1.visible = true
@@ -27,9 +29,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if not was_on_floor:
+			$coyote.start()
+			was_on_floor = true
+	
+	if is_on_floor():
+		able_to_jump = true
+		was_on_floor = false
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("up") and able_to_jump:
 		velocity.y = JUMP_VELOCITY
+		able_to_jump = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -53,3 +63,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	$Trug.visible = false
+
+
+func _on_coyote_timeout() -> void:
+	able_to_jump = false
