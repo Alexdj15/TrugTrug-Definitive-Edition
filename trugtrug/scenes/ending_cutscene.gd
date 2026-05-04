0f47ea79624c4fecba7999d570ef9fb422ba618b
Alexdj15 <alexo.dejong@outlook.com>
2026-05-04 21:57:37 +1200

    end cutscene finished

--- trugtrug/scenes/ending_cutscene.gd
+++ trugtrug/scenes/ending_cutscene.gd
@@ -3,20 +3,33 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$the_end.visible = false
 	$rapzer.visible = false
 	$rapzerbg.visible = false
+	$rapzer_label.visible = false
 	$dead_trug1.visible = false
 	$CanvasLayer/AnimationPlayer.play("fade_from_black")
 	$dead_trug1.visible = true
 	$trug_death_timer.start()
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_trug_death_timer_timeout() -> void:
 	$rapzer.visible = true
 	$rapzerbg.visible = true
+	$rapzer_label.visible = true
 	$CanvasLayer/AnimationPlayer.play("fade_to_rapzer")
+	$rapzer_timer.start()
+
+func _on_rapzer_timer_timeout() -> void:
+	$the_end.visible = true
+	$rapzer.visible = false
+	$rapzerbg.visible = false
+	$rapzer_label.visible = false
+	$dead_trug1.visible = false
+	$end_timer.start()
+
+func _on_end_timer_timeout() -> void:
+	pass
